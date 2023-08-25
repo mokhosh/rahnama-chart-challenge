a07f5d62f7af926ae4d80918ae275128d61be735
Mo Khosh <mskhoshnazar@gmail.com>
2023-08-25 13:38:54 +0330

    add seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,20 +3,87 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Challenge;
+use App\Models\Competition;
+use App\Models\Examiner;
+use App\Models\Participant;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // create 100 users
+         User::factory(100)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // create 1 competition
+        $competition = Competition::create(['title' => 'test competition']);
+
+        // create 2 groups
+        $firstGroup = $competition->groups()->create(['title' => 'first group']);
+        $secondGroup = $competition->groups()->create(['title' => 'second group']);
+
+        // create 4 fields and associate with groups
+        $firstField = $firstGroup->fields()->create(
+            ['title' => 'firstField'],
+            ['competition_id' => $competition->id],
+        );
+        $secondField = $firstGroup->fields()->create(
+            ['title' => 'secondField'],
+            ['competition_id' => $competition->id],
+        );
+        $thirdField = $secondGroup->fields()->create(
+            ['title' => '$thirdField'],
+            ['competition_id' => $competition->id],
+        );
+        $fourthField = $secondGroup->fields()->create(
+            ['title' => 'fourthField'],
+            ['competition_id' => $competition->id],
+        );
+
+        // create 5 age ranges
+        // challenges should also be created with this query
+        $firstField->age_ranges()->create([
+            'title' => '6-10',
+            'competition_id' => $competition->id,
+        ]);
+        $firstField->age_ranges()->create([
+            'title' => '11-16',
+            'competition_id' => $competition->id,
+        ]);
+        $firstField->age_ranges()->create([
+            'title' => '16-20',
+            'competition_id' => $competition->id,
+        ]);
+        $thirdField->age_ranges()->create([
+            'title' => '6-10',
+            'competition_id' => $competition->id,
+        ]);
+        $thirdField->age_ranges()->create([
+            'title' => '11-16',
+            'competition_id' => $competition->id,
+        ]);
+
+        // create 100 participants, 20 for each of the 5 challenges
+        foreach (range(1, 5) as $challengeId) {
+            foreach (range($challengeId * 20 - 19 , $challengeId * 20) as $userId) {
+                $challenge = Challenge::find($challengeId);
+
+                Participant::create([
+                    'user_id' => $userId,
+                    'competition_id' => $competition->id,
+                    'field_id' => $challenge->field_id,
+                    'challenge_id' => $challengeId,
+                ]);
+            }
+        }
+
+        // create 50 examiners
+        foreach (range(1, 50) as $participantId) {
+            Examiner::create([
+                'participant_id' => $participantId,
+            ]);
+        }
     }
 }
