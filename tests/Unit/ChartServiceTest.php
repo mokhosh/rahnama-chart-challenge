621ed36442df7e41694b084ab704721db02d0edb
Mo Khosh <mskhoshnazar@gmail.com>
2023-08-25 13:55:15 +0330

    update the expected format

--- tests/Unit/ChartServiceTest.php
+++ tests/Unit/ChartServiceTest.php
@@ -6,6 +6,39 @@ test('it can query chart data in the expected format', function () {
     $results = (new \App\Services\ChartService)->getParticipantCountByAgeRangeAndField();
 
     expect($results)->toBe([
-
+        [
+            'field' => 'first field',
+            'participants' => [
+                '6-10' => 20,
+                '11-16' => 20,
+                '16-20' => 20,
+            ],
+            'examiners' => [
+                '6-10' => 20,
+                '11-16' => 20,
+                '16-20' => 20,
+            ],
+        ],
+        [
+            'field' => 'second field',
+            'participants' => [],
+            'examiners' => [],
+        ],
+        [
+            'field' => 'third field',
+            'participants' => [
+                '6-10' => 20,
+                '11-16' => 20,
+            ],
+            'examiners' => [
+                '6-10' => 20,
+                '11-16' => 20,
+            ],
+        ],
+        [
+            'field' => 'fourth field',
+            'participants' => [],
+            'examiners' => [],
+        ],
     ]);
 });
