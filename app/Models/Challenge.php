bcedd00b45db3d372d195bc2d1b0afcc4064122c
Mo Khosh <mskhoshnazar@gmail.com>
2023-08-25 14:46:31 +0330

    add required relationship

--- app/Models/Challenge.php
+++ app/Models/Challenge.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Challenge extends Model
 {
     use HasFactory;
+
+    public function age_range()
+    {
+        return $this->belongsTo(AgeRange::class, 'age_id');
+    }
 }
