4de19bef417a70d9fba7ce1b3174e6b9b06f334e
Mo Khosh <mskhoshnazar@gmail.com>
2023-08-25 12:47:42 +0330

    comment test steps

--- tests/Unit/ChartServiceTest.php
+++ tests/Unit/ChartServiceTest.php
@@ -1,5 +1,14 @@
 <?php
 
-test('example', function () {
+test('it can query chart data in the expected format', function () {
+    // create 1 competition
+    // create 2 groups
+    // create 4 fields and associate with groups
+    // create 6 age ranges
+    // create 10 challenges
+    // create 100 participants
+    // create 50 examiners
+    // run the service
+    // expect the format and the data
     expect(true)->toBeTrue();
 });
